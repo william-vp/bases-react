73a94265cfeb999f3c83c531fde421cd04e2418a
WilliamVP-25 <wavp25@gmail.com>
2022-02-01 07:43:07 -0500

    Routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import {Suspense} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {Route, Routes, NavLink, Navigate} from 'react-router-dom';
 import {RoutesNav} from '../lazyload/router/Routes'
