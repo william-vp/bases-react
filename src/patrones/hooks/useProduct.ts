8d3ed63b88f7c82897ebfff4e8e435688eb54610
WilliamVP-25 <wavp25@gmail.com>
2022-02-15 07:53:41 -0500

    Control props implementacion alternativo

--- src/patrones/hooks/useProduct.ts
+++ src/patrones/hooks/useProduct.ts
@@ -10,15 +10,7 @@ interface useProductArgs {
 export const useProduct = ({onChange, product, value = 0}: useProductArgs) => {
     const [counter, setCounter] = useState(0)
     
-    const isControlled= useRef(!!onChange)
-    
     const increment = (value: number) => {
-        if (isControlled.current) {
-            return onChange!({
-                count: value, product
-            })
-        }
-        console.log('is controlled', isControlled.current)
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue)
         onChange && onChange({count: newValue, product})
--- src/patrones/hooks/useShoppingCart.ts
+++ src/patrones/hooks/useShoppingCart.ts
@@ -6,22 +6,12 @@ export const useShoppingCart = () => {
 
     const onProductCountChange = ({count, product}: { count: number, product: Product }) => {
         let newShoppingCart = {...shoppingCart}
-        const productInCart: ProductInCart = newShoppingCart[product.id] || {...product, count: 0}
-        if (Math.max(productInCart.count + count, 0) >= 1) {
-            productInCart.count += count
-            setShoppingCart({...newShoppingCart, [product.id]: productInCart})
-        } else {
-            delete newShoppingCart[product.id]
-            setShoppingCart(newShoppingCart)
-        }
-
-        /*let newShoppingCart = {...shoppingCart}
         if (count == 0) {
             delete newShoppingCart[product.id]
         } else {
             newShoppingCart[product.id] = {...product, count}
         }
-        setShoppingCart(newShoppingCart)*/
+        setShoppingCart(newShoppingCart)
     }
 
     return {
